b36ef9c2e1cd169bb65f1f2ec5f4244789f6b294
Chris O'Connell <chris@chrisoconnell.info>
2012-12-09 14:53:42 -0700

    added more unit tests

--- test/CaoHtmlTableTest/View/Helper/CaoHtmlTableTest.php
+++ test/CaoHtmlTableTest/View/Helper/CaoHtmlTableTest.php
@@ -3,6 +3,7 @@
 namespace CaoHtmlTableTest\View\Helper;
 
 use CaoHtmlTable\View\Helper\CaoHtmlTable;
+use CaoHtmlTable\Model\Table;
 use Zend\View\Renderer\PhpRenderer as View;
 
 class CaoHtmlTableTest extends \PHPUnit_Framework_TestCase
@@ -96,5 +97,110 @@ class CaoHtmlTableTest extends \PHPUnit_Framework_TestCase
         foreach(array('h1', 'h2') as $header) {
             $this->assertContains('<th>' . $header . '</th>', $table);
         }
+    }
+
+    public function testTableFromEmptyInstance()
+    {
+        $table = $this->helper->__invoke(new Table());
+
+        $contains = array('<table>', '</table>', '<tbody>', '</tbody>');
+        $notContains = array('<thead>', '</thead>', '<tr>', '</tr>', '<th>', '</th>', '<td>', '</td>');
+
+        foreach ($contains as $contain) {
+            $this->assertContains($contain, $table);
+        }
+        foreach ($notContains as $notContain) {
+            $this->assertNotContains($notContain, $table);
+        }
+    }
+
+    public function testTableFromInstanceWithData()
+    {
+        $data = array(
+            array('c1r1', 'c2r1'),
+            array('c1r2', 'c2r2'),
+        );
+        $table = $this->helper->__invoke(new Table($data));
+
+        $contains = array('<table>', '</table>', '<tbody>', '</tbody>', '<tr>', '</tr>', '<td>', '</td>');
+        $notContains = array('<thead>', '</thead>', '<th>', '</th>');
+
+        foreach ($contains as $contain) {
+            $this->assertContains($contain, $table);
+        }
+        foreach ($notContains as $notContain) {
+            $this->assertNotContains($notContain, $table);
+        }
+
+        // Confirm td tag was generated for each element.
+        foreach($data as $row) {
+            foreach($row as $element) {
+                $this->assertContains('<td>' . $element . '</td>', $table);
+            }
+        }
+    }
+
+    public function testTableFromInstanceWithCaption()
+    {
+        $data = array(
+            array('c1r1', 'c2r1'),
+            array('c1r2', 'c2r2'),
+        );
+        $caoTable = new Table($data);
+        $caoTable->setCaption('My Caption');
+        $table = $this->helper->__invoke($caoTable);
+
+        // Confirm caption tag was generated.
+        $this->assertContains('<caption>My Caption</caption>', $table);
+    }
+
+    public function testTableFromInstanceWithSetHeaderRow()
+    {
+        $data = array(
+            array('c1r1', 'c2r1'),
+            array('c1r2', 'c2r2'),
+        );
+        $headers = array('h1', 'h2');
+        $caoTable = new Table($data);
+        $caoTable->setHeaderRow($headers);
+        $table = $this->helper->__invoke($caoTable);
+
+        // Confirm th tag was generated for each header.
+        foreach($headers as $header) {
+            $this->assertContains('<th>' . $header . '</th>', $table);
+        }
+    }
+
+    public function testTableFromInstanceWithIndexedSetHeaderRow()
+    {
+        $data = array(
+            array('h1' => 'c1r1', 'h2' => 'c2r1'),
+            array('h1' => 'c1r2', 'h2' => 'c2r2'),
+        );
+        $headers = array('h1' => 'Header 1', 'h2' => 'Header 2');
+        $caoTable = new Table($data);
+        $caoTable->setHeaderRow($headers);
+        $table = $this->helper->__invoke($caoTable);
+
+        // Confirm th tag was generated for each header.
+        foreach($headers as $index => $header) {
+            $this->assertContains('<th>' . $header . '</th>', $table);
+            $this->assertNotContains($index, $table);
+        }
+    }
+
+    public function testTableFromInstanceWithSetAttributes()
+    {
+        $data = array(
+            array('h1' => 'c1r1', 'h2' => 'c2r1'),
+            array('h1' => 'c1r2', 'h2' => 'c2r2'),
+        );
+        $attributes = array('class' => 'selected', 'name' => 'list');
+        $caoTable = new Table($data);
+        $caoTable->setAttributes($attributes);
+        $table = $this->helper->__invoke($caoTable);
+
+        $this->assertContains('class="selected"', $table);
+        $this->assertContains('name="list"', $table);
     }
 }
